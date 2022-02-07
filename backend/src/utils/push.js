5169ae82f88e6d25c137431478f690739e8eebf4
luiztools <luizfduartejr@gmail.com>
2022-02-07 17:46:36 -0300

    bugfix usuário sem push token

--- backend/src/utils/push.js
+++ backend/src/utils/push.js
@@ -4,7 +4,7 @@ const settingsRepository = require('../repositories/settingsRepository');
 module.exports = async (settings, body, title = 'Beholder Notification', data = {}) => {
 
     if (!settings) throw new Error(`The settings object is required to send push notifications!`);
-    if (!settings.pushToken) throw new Error(`The push token at settings is not defined!`);
+    if (!settings.pushToken) return false;
 
     const response = await axios.post('https://exp.host/--/api/v2/push/send', {
         to: settings.pushToken,
