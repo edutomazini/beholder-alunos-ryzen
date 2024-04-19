c0a1cb3a32eba9a3af822c46183f518f459afc8e
LuizTools <luizfduartejr@gmail.com>
2024-04-19 19:44:20 -0300

    index.js React v18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Routes from './routes';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Routes />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
